abb790e7b3d4090c6192e224a6ddc5717787e965
Lívia Brasil <liviabrasil@id.uff.br>
2021-07-29 11:27:56 -0300

    "fix meta viewport"

--- src/pages/newTest.js
+++ src/pages/newTest.js
@@ -36,6 +36,7 @@ export default function NewTest (){
             <TextField  select
                         label="Categoria"
                         value={currencies}
+                        multiple={false}
                         variant="outlined"
                         SelectProps={{
                             native: true,
